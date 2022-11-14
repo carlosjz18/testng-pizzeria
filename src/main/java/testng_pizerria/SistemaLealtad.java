6b67c6494074a2bd046f6848daf60dc53c2e84e3
Carlos Jaimez <carlosjaimez18@outlook.com>
2022-11-13 22:12:48 -0600

    Pruebas unitarias iniciales

---
+++ src/main/java/testng_pizerria/SistemaLealtad.java
@@ -0,0 +1,49 @@
+package testng_pizerria;
+
+public class SistemaLealtad {
+
+    private boolean estatusTajeta;
+    private int saldoPuntos;
+    private boolean estatusCupon;
+
+
+    public boolean getEstatusTarjeta() {
+        return this.estatusTajeta;
+    }
+
+    public void setEstatusTarjeta(boolean estatusTarjeta) {
+        this.estatusTajeta = estatusTarjeta;
+    }
+
+    public int getSaldoPuntos() {
+        return saldoPuntos;
+    }
+
+    public void setSaldoPuntos(int saldoPuntos) {
+        this.saldoPuntos = saldoPuntos;
+    }
+
+    public boolean getEstatusCupon() {
+        return estatusCupon;
+    }
+
+    public void setEstatusCupon(boolean estatusCupon) {
+        this.estatusCupon = estatusCupon;
+    }
+
+    public String mostrarProductosExtras() {
+        return null;
+    }
+
+    public void usarCupon(String respuesta) {
+
+        if (respuesta.equals("Y")) {
+
+        } else if (respuesta.equals("N")) {
+
+        } else {
+
+        }
+    }
+
+}
---
+++ src/test/java/CommonMethods.java
@@ -0,0 +1,13 @@
+public class CommonMethods {
+    
+    public void redirectStdOutput(String stdOutputPath) {
+    }
+
+    public String readFileContents(String stdOutputPath) {
+        return null;
+    }
+
+    public void redirectStdInput(String stdInputPath) {
+    }
+
+}
